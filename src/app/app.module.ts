b0defe4b5cfc0a4d6a5dc93b248b8792225fc51f
Jon Nakkerud <jonnakkerud@Jons-MacBook-Air.local>
2019-11-17 18:18:43 -0800

    add firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+
 import { CoreModule } from './core/core.module';
 import { CollectionListModule } from './collection-list/collection-list.component';
 import { EditCollectionModule } from './edit-collection/edit-collection.component';
 import { CreateCollectionModule } from './create-collection/create-collection.component';
 import { GenerateLinkModule } from './generate-link/generate-link.component';
 import { GeneratedFormModule } from './generated-form/generated-form.component';
+
+// https://www.learnhowtoprogram.com/javascript/angular-extended/firebase-introduction-and-setup
+import { masterFirebaseConfig } from './api-keys';
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
@@ -19,6 +30,7 @@ import { GeneratedFormModule } from './generated-form/generated-form.component';
   imports: [
     BrowserModule,
     CoreModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     CollectionListModule,
     EditCollectionModule,
     CreateCollectionModule,
