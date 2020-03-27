d057b1623d7e6e6292503506430c0a0cddc48c64
Jon Nakkerud <jonnakkerud@Jons-MacBook-Air.local>
2020-03-27 10:31:42 -0700

    name change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'duvola'`, () => {
+  it(`should have as title 'fireform'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('duvola');
+    expect(app.title).toEqual('fireform');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('duvola app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('fireform app is running!');
   });
 });
