d4e6c262d1c97fcd31f4fee0d4def2f9b1bfcb7a
Jon Nakkerud <jonnakkerud@Jons-MacBook-Air.local>
2020-04-06 10:22:08 -0700

    Cleanup data in downloadCSV function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,26 +2,48 @@ import * as functions from 'firebase-functions'
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+import { firestore } from 'firebase-admin';
+
 const json2csv = require("json2csv").parse;
+
+function convert(data: any) {
+    const validData: {[index: string]: any} = {};
+
+    Object.keys(data).forEach(key => {
+        let val = data[key];
+        if (val instanceof firestore.Timestamp) {
+            const ts = new firestore.Timestamp(val.seconds, val.nanoseconds).toDate();
+            if (key === 'timestamp') {
+                val = ts.toISOString();
+            } else {
+                val = ts.toLocaleDateString();
+            }
+        } else if (Array.isArray(val)) {
+            // array values: comma delimted quotes
+            val = val.join();
+        }
+        validData[key] = val;
+    });
+
+    return validData;
+}
 
 // See basic example: https://github.com/firebase/functions-samples/tree/master/typescript-getting-started
 export const downloadCSV = functions.https.onCall((data, context) => {
 
     const collectionId = data.collectionId;
-    console.log('collectionId:'+collectionId);
 
     const db = admin.firestore()
     return db.collection(`formdata/${collectionId}/data`)
     .get() 
     .then((querySnapshot: any[]) => {
        
-       const csvData: any[] = []
+       const csvData: any[] = [];
 
        querySnapshot.forEach(doc => {
-           // TODO convert Timestamp to Date
-           csvData.push( doc.data() )
+           const d = doc.data();
+           csvData.push( convert(d) );
        });
-
        
        return json2csv(csvData);
     })
